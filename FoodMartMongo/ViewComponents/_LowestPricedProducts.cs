13b72c3f8d1fe27cbdffa41ceb54d9d9dea9a37f
ozgekuzucu <ozge.zz53@gmail.com>
2025-05-23 01:20:06 +0300

    .

---
+++ FoodMartMongo/ViewComponents/_LowestPricedProducts.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodMartMongo.ViewComponents
+{
+	public class _LowestPricedProducts : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
---
+++ FoodMartMongo/ViewComponents/_SvgComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodMartMongo.ViewComponents
+{
+	public class _SvgComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
